bbf344348157e47f77acd842379002f318fad524
JackieChu <mayday.jacky@gmail.com>
2019-08-03 23:25:20 +0800

    Auto-generated commit

--- pages-index-index-module-es5.js
+++ pages-index-index-module-es5.js
@@ -284,17 +284,17 @@ var BannerComponent = /** @class */ (function () {
         this.slider = new ngx_slider__WEBPACK_IMPORTED_MODULE_2__["Slider"]();
         this.slideItems = [
             {
-                src: '/assets/images/banner2.jpg',
+                src: 'assets/images/banner2.jpg',
                 title: 'FXL INTERNATIONAL CONSULTING',
                 info: ['您可信賴的', '守護者']
             },
             {
-                src: '/assets/images/banner1.jpg',
+                src: 'assets/images/banner1.jpg',
                 title: 'FXL INTERNATIONAL CONSULTING',
                 info: ['整合專業', '創新服務']
             },
             {
-                src: '/assets/images/banner3.jpg',
+                src: 'assets/images/banner3.jpg',
                 title: 'FXL INTERNATIONAL CONSULTING',
                 info: ['多元化顧問團體']
             }
